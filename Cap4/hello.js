696e7b0912a3e64f439e3b4214df915b33531dcc
Aldo Amati <aldilo08@hotmail.com>
2025-05-05 15:19:29 -0300

    upd: rotas

--- Cap4/hello.js
+++ Cap4/hello.js
@@ -1,12 +1,22 @@
-var http = require("http")
+var http = require("http");
+var url = require("url");
 
 var callback = function (request, response) {
-    response.writeHead(200, {"Content-Type": "text/plain"})
-    response.end("Hello World Node Callback\n")
-}
+  response.writeHead(200, { "Content-Type": "text/plain" });
 
-var server = http.createServer(callback)
+  // Fazer parser de URL separando rotas
+  var parts = url.parse(request.url);
+  if (parts.path == "/") {
+    response.end("Site na raiz.");
+  } else if (parts.path == "/carros") {
+    response.end("Você entrou na rota de /carros.");
+  } else {
+    response.end("Rota inválida: " + parts.path);
+  }
+};
 
-server.listen(3000)
+var server = http.createServer(callback);
 
-console.log("Servidor iniciado em http://localhost:3000")
+server.listen(3000);
+
+console.log("Servidor iniciado em http://localhost:3000");
