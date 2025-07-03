cac2d83277cf0b64888daedff3507a0f58d83aaa
Aldo Amati <aldilo08@hotmail.com>
2025-07-03 19:27:31 -0300

    new: exercise

--- Cap7/CarroDB.js
+++ Cap7/CarroDB.js
@@ -28,22 +28,31 @@ class CarroDB {
   }
 
   static getCarrosByTipo(tipo, callback) {
-    let connection = CarroDB.connect()
+    let connection = CarroDB.connect();
     // Cria uma consulta
-    let sql = "select id,nome,tipo from carro where tipo = '" + tipo + "'"
+    let sql = "select id,nome,tipo from carro where tipo = '" + tipo + "'";
     let query = connection.query(sql, function (error, results, fields) {
-        if(error) throw error
-        callback(results)
-    })
-    console.log(query.sql)
-    connection.end()
+      if (error) throw error;
+      callback(results);
+    });
+    console.log(query.sql);
+    connection.end();
   }
 
   static getCarrosById(id, callback) {
-    let connection = CarroDB.connect()
-    let sql = "select * from carro where id=?"
-    let query = connection.query(sql, function(error, results, fields) {
-        
-    })
+    let connection = CarroDB.connect();
+    let sql = "select * from carro where id=?";
+    let query = connection.query(sql, id, function (error, results, fields) {
+      if (error) throw error;
+      if (results.length === 0) {
+        console.log("Nenhum carro encontrado.");
+        return;
+      }
+      // Encontrou o carro
+      let carro = results[0];
+      callback(carro)
+    });
+    console.log(query.sql)
+    connection.end()
   }
 }
