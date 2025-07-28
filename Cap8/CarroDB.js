fe38f716da1fe1eb7663a89417a6df0f84a0ad0b
Aldo Amati <aldilo08@hotmail.com>
2025-07-27 21:37:07 -0300

    new: exercise

---
+++ Cap8/CarroDB.js
@@ -0,0 +1,115 @@
+var mysql = require("mysql2");
+
+class CarroDB {
+  // Função para conectar no banco de dados
+  static connect() {
+    var connection = mysql.createConnection({
+      host: "localhost",
+      user: "livro",
+      password: "livro123",
+      database: "livro",
+    });
+    connection.connect();
+    return connection;
+  }
+
+  // Retorna a lista de carros
+  static getCarros(callback) {
+    let connection = CarroDB.connect();
+    // Cria uma consulta
+    let sql = "select * from carro";
+    let query = connection.query(sql, function (error, results, fields) {
+      if (error) throw error;
+      // Retorna os dados pela função de callback
+      callback(results);
+    });
+    console.log(query.sql);
+    connection.end();
+  }
+
+  static getCarrosByTipo(tipo, callback) {
+    let connection = CarroDB.connect();
+    // Cria uma consulta
+    let sql = "select id,nome,tipo from carro where tipo = '" + tipo + "'";
+    let query = connection.query(sql, function (error, results, fields) {
+      if (error) throw error;
+      callback(results);
+    });
+    console.log(query.sql);
+    connection.end();
+  }
+
+  static getCarrosById(id, callback) {
+    let connection = CarroDB.connect();
+    let sql = "select * from carro where id=?";
+    let query = connection.query(sql, id, function (error, results, fields) {
+      if (error) throw error;
+      if (results.length === 0) {
+        console.log("Nenhum carro encontrado.");
+        return;
+      }
+      // Encontrou o carro
+      let carro = results[0];
+      callback(carro);
+    });
+    console.log(query.sql);
+    connection.end();
+  }
+
+  // Salva carro no banco de dados
+  // Recebe o JSON com dados do carro como parâmetro
+  static save(carro, callback) {
+    let connection = CarroDB.connect();
+    // Insere o carro
+    let sql = "insert into carro set ? ";
+    let query = connection.query(sql, carro, function (error, results, fields) {
+      if (error) throw error;
+      // Atualiza o objeto carro como parâmetro com o "id" inserido
+      carro.id = results.insertId;
+      // Retorna o carro pela função de callback
+      callback(carro);
+    });
+    console.log(query.sql);
+    connection.end();
+  }
+
+  static update(carro, callback) {
+    let connection = CarroDB.connect();
+    let sql = "update carro set ? where id = ?";
+    let id = carro.id;
+    let query = connection.query(
+      sql,
+      [carro, id],
+      function (error, results, fields) {
+        if (error) throw error;
+        callback(carro);
+      }
+    );
+    console.log(query.sql);
+    connection.end();
+  }
+
+  static delete(carro, callback) {
+    let connection = CarroDB.connect();
+    let sql = "delete from carro where id = ?";
+    let id = carro.id;
+    let query = connection.query(sql, id, function (error, results, fields) {
+      if (error) throw error;
+      callback(carro);
+    });
+    console.log(query.sql);
+    connection.end();
+  }
+
+  static deleteById(id, callback) {
+    let connection = CarroDB.connect();
+    let sql = "delete from carro where id = ?";
+    let query = connection.query(sql, id, function (error, results, fields) {
+      if (error) throw error;
+      callback(results.affectedRows);
+    });
+    console.log(query.sql);
+    connection.end();
+  }
+}
+module.exports = CarroDB;
---
+++ Cap8/app.js
@@ -0,0 +1,43 @@
+var http = require("http");
+var url = require("url");
+
+const CarroDb = require("./CarroDB");
+
+// Consulta os carros pelo tipo e retorn o JSON na resposta
+function getCarros(response, tipo) {
+  // Busca carros no banco
+  CarroDb.getCarrosByTipo(tipo, function (carros) {
+    // Converte array de carros para JSON
+    var json = JSON.stringify(carros);
+    // ENvia JSON como resposta
+    response.end(json);
+  });
+}
+
+// FUnção de callback para o servidor HTTP
+function callback(request, response) {
+  // Faz o parser do URL separando o caminha (path)
+  var parts = url.parse(request.url);
+  var path = parts.path;
+  // Configura o tipo de retorno para application/json
+  response.writeHead(200, {
+    "Content-Type": "application/json; charset=utf-8",
+  });
+  // Verifica o path
+  if (path == "/carros/classicos") {
+    getCarros(response, "classicos");
+  } else if (path == "/carros/esportivos") {
+    getCarros(response, "esportivos");
+  } else if (path == "/carros/luxo") {
+    getCarros(response, "luxo");
+  } else {
+    response.end("Not found: " + path);
+  }
+}
+
+// Cria um servidor HTTP que vai responder "Hello World" a todas as requisições
+var server = http.createServer(callback);
+// Porta que o servidor vai escutar
+server.listen(3000);
+// Mensagem ao iniciar o servidor
+console.log("Servidor iniciando em http://localhost:3000");
