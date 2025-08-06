9ce4540e384f906080e27fc674e0c30a85d80699
Aldo Amati <aldilo08@hotmail.com>
2025-08-05 21:06:31 -0300

    new: chapter

---
+++ Cap9/app.js
@@ -0,0 +1,14 @@
+let express = require("express");
+let app = express();
+
+// Configura uma rota na raiz
+app.get("/", function (req, res) {
+  res.send("Hello World Express");
+});
+
+// Inicia o servidro
+let server = app.listen(3000, function () {
+  let host = server.address().address;
+  let port = server.address().port;
+  console.log("Servidor iniciado em http://%s:%s", host, port);
+});
