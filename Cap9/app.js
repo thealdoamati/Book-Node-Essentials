4bf9b4369798d4885b4ab10c990155259a30c3ca
Aldo Amati <aldilo08@hotmail.com>
2025-08-26 10:51:53 -0300

    new: reqs

--- Cap9/app.js
+++ Cap9/app.js
@@ -1,28 +1,42 @@
 let express = require("express");
 let app = express();
 
+// Permire ler os parâmetros do tipo form-urlencoded
+var bodyParser = require("body-parser");
+app.use(bodyParser.urlencoded({ extended: false }));
+
+//Configurando para ler tipo application/json
+app.use(bodyParser.json())
+
 // Configura uma rota na raiz
 app.get("/", function (req, res) {
-  res.json({nome: 'Ricardo', sobrenome: 'Lecheta'})
+  res.json({ nome: "Ricardo", sobrenome: "Lecheta" });
 });
 
 app.get("/pessoa", function (req, res) {
-	let nome = req.query.nome;
-	let sobrenome = req.query.sobrenome;
-	res.status(200).type("text")
-	res.send(nome + " " + sobrenome)
-})
+  let nome = req.query.nome;
+  let sobrenome = req.query.sobrenome;
+  res.status(200).type("text");
+  res.send(nome + " " + sobrenome);
+});
 
 app.get("/pessoa/:id", function (req, res) {
-	let id = req.params.id;
-	res.send("Buscar a pessoa " + id);
-})
+  let id = req.params.id;
+  res.send("Buscar a pessoa " + id);
+});
 
 app.get("/pessoa/nome/:nome/sobrenome/:sobrenome", function (req, res) {
-	let nome = req.params.nome
-	let sobrenome = req.params.sobrenome
-	res.send(nome + " " + sobrenome)
-})
+  let nome = req.params.nome;
+  let sobrenome = req.params.sobrenome;
+  res.send(nome + " " + sobrenome);
+});
+
+// POST
+app.post("/pessoa", function (req, res) {
+  let nome = req.body.nome;
+  let sobrenome = req.body.sobrenome;
+  res.send(nome + " " + sobrenome);
+});
 
 // Inicia o servidro
 let server = app.listen(3000, function () {
