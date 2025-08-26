af1962c7d7ce0165322f9995b0192685c2104fc1
Aldo Amati <aldilo08@hotmail.com>
2025-08-25 22:07:05 -0300

    new: params

--- Cap9/app.js
+++ Cap9/app.js
@@ -3,8 +3,20 @@ let app = express();
 
 // Configura uma rota na raiz
 app.get("/", function (req, res) {
-  res.send("Hello World Express");
+  res.json({nome: 'Ricardo', sobrenome: 'Lecheta'})
 });
+
+app.get("/pessoa", function (req, res) {
+	let nome = req.query.nome;
+	let sobrenome = req.query.sobrenome;
+	res.status(200).type("text")
+	res.send(nome + " " + sobrenome)
+})
+
+app.get("/pessoa/:id", function (req, res) {
+	let id = req.params.id;
+	res.send("Buscar a pessoa " + id);
+})
 
 // Inicia o servidro
 let server = app.listen(3000, function () {
