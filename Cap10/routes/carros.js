ca78690e3b94cacdb3697545053f3189b1a139da
Aldo Amati <aldilo08@hotmail.com>
2025-10-16 20:09:05 -0300

    upd: endpoint

--- Cap10/routes/carros.js
+++ Cap10/routes/carros.js
@@ -2,23 +2,23 @@ let express = require("express");
 const router = express.Router();
 const CarroDb = require("../model/CarroDB");
 
-// GET em /carros
-router.get("/carros", function (req, res) {
+// GET em
+router.get("/", function (req, res) {
   CarroDb.getCarros(function (carros) {
     res.json(carros);
   });
 });
 
 // GET em carros/id (Tem que vir antes do tipo, pois o id é number)
-router.get("/carros/:id", function (req, res) {
+router.get("/:id", function (req, res) {
   let id = req.params.id;
   CarroDb.getCarrosById(id, function (carros) {
     res.json(carros);
   });
 });
 
-// DELETE em /carros/id
-router.delete("/carros/:id", function (req, res) {
+// DELETE em /id
+router.delete("/:id", function (req, res) {
   let id = req.params.id;
   console.log("deletar carro " + id);
   CarroDb.delete(id, function (affectedRows) {
@@ -27,21 +27,21 @@ router.delete("/carros/:id", function (req, res) {
 });
 
 // GET em carros/esportivos
-router.get("/carros/:tipo", function (req, res) {
+router.get("/:tipo", function (req, res) {
   let tipo = req.params.tipo;
   CarroDb.getCarrosByTipo(tipo, function (carros) {
     res.json(carros);
   });
 });
 
-router.post("/carros", function (req, res) {
+router.post("/", function (req, res) {
   let carro = req.body;
   CarroDb.save(carro, function (carro) {
     res.json(carro);
   });
 });
 
-router.put("/carros", function (req, res) {
+router.put("/", function (req, res) {
   let carro = req.body;
   CarroDb.update(carro, function (carro) {
     // res.json(carro);
